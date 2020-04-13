b1a88df8fdf9134540f5395d9050ed70ae56ba79
anisapatel <anisa_patel@live.co.uk>
2020-04-13 23:29:24 +0100

    initial commit

---
+++ backend/db/data/dev-data/recipes.js
@@ -0,0 +1,81 @@
+module.exports = [
+  {
+    chef: "Anisa",
+    recipes: [
+      {
+        RecipeName: "Spaghetti Bolognese",
+        cuisine: "Italian",
+        Ingredients: ["Tomato", "Spaghetti", "Parmesan", "Mince"],
+        Method: [
+          "Heat a large saucepan over a medium heat. Add a tablespoon of olive oil and once hot add the beef mince and a pinch of salt and pepper. Cook the mince until well browned over a medium-high heat (be careful not to burn the mince. It just needs to be a dark brown colour). Once browned, transfer the mince to a bowl and set aside.",
+          "Add another tablespoon of oil to the saucepan you browned the mince in and turn the heat to medium. Add the onions and a pinch of salt and fry gently for 5-6 minutes, or until softened and translucent. Add the garlic and cook for another 2 minutes. Add the grated carrot then pour the mince and any juices in the bowl back into the saucepan.",
+          "Add the tomatoes to the pan and stir well to mix. Pour in the stock, bring to a simmer and then reduce the temperature to simmer gently for 45 minutes, or until the sauce is thick and rich. Taste and adjust the seasoning as necessary.",
+          "When ready to cook the spaghetti, heat a large saucepan of water and add a pinch of salt. Cook according to the packet instructions. Once the spaghetti is cooked through, drain and add to the pan with the bolognese sauce. Mix well and serve.",
+        ],
+      },
+    ],
+  },
+  {
+    chef: "John",
+    recipes: [
+      {
+        RecipeName: "Lasagne",
+        cuisine: "Italian",
+        Ingredients: ["Tomato", "Pasta sheet", "Parmesan", "Mince"],
+        Method: [
+          "To make the meat sauce, heat 2 tbsp olive oil in a frying pan and cook 750g lean beef mince in two batches for about 10 mins until browned all over.",
+          "Finely chop 4 slices of prosciutto from a 90g pack, then stir through the meat mixture.",
+          "Pour over 800g passata or half our basic tomato sauce recipe and 200ml hot beef stock. Add a little grated nutmeg, then season.",
+          "Bring up to the boil, then simmer for 30 mins until the sauce looks rich.",
+          "Heat oven to 180C/fan/160C/gas 4 and lightly oil an ovenproof dish (about 30 x 20cm).",
+          "Spoon one third of the meat sauce into the dish, then cover with some fresh lasagne sheets from a 300g pack. Drizzle over roughly 130g ready-made or homemade white sauce.",
+          "Repeat until you have 3 layers of pasta. Cover with the remaining 390g white sauce, making sure you can’t see any pasta poking through.",
+          "Scatter 125g torn mozzarella over the top.",
+          "Arrange the rest of the prosciutto on top. Bake for 45 mins until the top is bubbling and lightly browned.",
+        ],
+      },
+    ],
+  },
+  {
+    chef: "Muhammad",
+    recipes: [
+      {
+        RecipeName: "Lasagne",
+        cuisine: "Italian",
+        Ingredients: ["Tomato", "Pasta sheet", "Parmesan", "Mince"],
+        Method: [
+          "To make the meat sauce, heat 2 tbsp olive oil in a frying pan and cook 750g lean beef mince in two batches for about 10 mins until browned all over.",
+          "Finely chop 4 slices of prosciutto from a 90g pack, then stir through the meat mixture.",
+          "Pour over 800g passata or half our basic tomato sauce recipe and 200ml hot beef stock. Add a little grated nutmeg, then season.",
+          "Bring up to the boil, then simmer for 30 mins until the sauce looks rich.",
+          "Heat oven to 180C/fan/160C/gas 4 and lightly oil an ovenproof dish (about 30 x 20cm).",
+          "Spoon one third of the meat sauce into the dish, then cover with some fresh lasagne sheets from a 300g pack. Drizzle over roughly 130g ready-made or homemade white sauce.",
+          "Repeat until you have 3 layers of pasta. Cover with the remaining 390g white sauce, making sure you can’t see any pasta poking through.",
+          "Scatter 125g torn mozzarella over the top.",
+          "Arrange the rest of the prosciutto on top. Bake for 45 mins until the top is bubbling and lightly browned.",
+        ],
+      },
+    ],
+  },
+  {
+    chef: "Susie",
+    recipes: [
+      {
+        RecipeName: "Lasagne",
+        cuisine: "Italian",
+        Ingredients: ["Tomato", "Pasta sheet", "Parmesan", "Mince"],
+        Method: [
+          "To make the meat sauce, heat 2 tbsp olive oil in a frying pan and cook 750g lean beef mince in two batches for about 10 mins until browned all over.",
+          "Finely chop 4 slices of prosciutto from a 90g pack, then stir through the meat mixture.",
+          "Pour over 800g passata or half our basic tomato sauce recipe and 200ml hot beef stock. Add a little grated nutmeg, then season.",
+          "Bring up to the boil, then simmer for 30 mins until the sauce looks rich.",
+          "Heat oven to 180C/fan/160C/gas 4 and lightly oil an ovenproof dish (about 30 x 20cm).",
+          "Spoon one third of the meat sauce into the dish, then cover with some fresh lasagne sheets from a 300g pack. Drizzle over roughly 130g ready-made or homemade white sauce.",
+          "Repeat until you have 3 layers of pasta. Cover with the remaining 390g white sauce, making sure you can’t see any pasta poking through.",
+          "Scatter 125g torn mozzarella over the top.",
+          "Arrange the rest of the prosciutto on top. Bake for 45 mins until the top is bubbling and lightly browned.",
+        ],
+      },
+    ],
+  },
+];
---
+++ backend/db/data/test-data/users.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    username: "Anisa",
+    password: "password",
+    followers: ["Muhammad", "John", "Susie"],
+    following: ["Muhammad", "John", "Susie"],
+  },
+  {
+    username: "Muhammad",
+    password: "password",
+    followers: ["Anisa", "John"],
+    following: ["Muhammad", "John", "Susie"],
+  },
+  {
+    username: "John",
+    password: "password",
+    followers: ["Anisa", "Muhammad", "Susie"],
+    following: ["Muhammad", "John", "Susie"],
+  },
+  {
+    username: "Susie",
+    password: "password",
+    followers: ["Anisa", "John", "Susie"],
+    following: ["Muhammad", "John", "Susie"],
+  },
+];
